2a8c7534232b2eee1c0a52e1b2aef65352dec458
ilyas bikarnaf <ilyas.bikarnaf@gmail.com>
2025-02-22 21:42:06 +0100

    create date picker

---
+++ date-picker/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import DatePicker from "./DatePicker";
+import "./styles.css";
+
+export default function App() {
+ 
+
+  const [value, setValue] = useState(new Date())
+  
+  return <DatePicker value={value} onChange={setValue}/>;
+}
+
+// month day-th, year
